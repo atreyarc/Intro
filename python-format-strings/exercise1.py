af98cd48ca621b4317b583b449e948f23430d11d
atreyarc <atreya-roy.chowdhury@ch.abb.com>
2020-11-09 22:20:19 +0100

    string formatting ex. 1 complete.

--- python-format-strings/exercise1.py
+++ python-format-strings/exercise1.py
@@ -1,1 +1,20 @@
-test = input('is this a test?')
+#this is a verbatim string. Note the string openers ' ' '
+tenth_string = '''A literal string
+on more than one line
+sometimes known as a verbatim string'''
+
+eleventh_string = """Another literal string
+     on more than one line
+using double quotes"""
+
+print(tenth_string)
+print(eleventh_string)
+
+#note the use of sep and end commands.
+first = 'Conrad'
+second = 'Grant'
+third = 'Bob'
+print(first, second)
+print(first, second, third)
+print(first, second, third, sep='-')
+print(first, second, third, sep='-', end='.')
