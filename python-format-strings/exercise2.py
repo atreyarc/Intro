303effbca6d80b1b9701bb98d2e04b51dba950fd
atreyarc <atreya-roy.chowdhury@ch.abb.com>
2020-11-10 22:55:37 +0100

    final string ex2

--- python-format-strings/exercise2.py
+++ python-format-strings/exercise2.py
@@ -21,11 +21,35 @@ print(location.count('s'))
 
 print(len('how many letters in this string?')) """
 
-message = 'racecar'
+""" message = 'racecar'
 print(message.startswith('r'))
 print(message.startswith('a'))
 print(message.startswith('ra'))
 
 print(message.endswith('r'))
 print(message.endswith('a'))
-print(message.endswith('ar'))
+print(message.endswith('ar')) """
+
+
+""" message = 'The quick brown fox jumps over the lazy dog'
+print(message.find('qu'))
+
+print(message.find('t'))
+print(message.find('T'))
+print(message.find('0')) """
+""" 
+message = '    middle     '
+print('.' + message.lstrip() + '.')
+print('.' + message.rstrip() + '.')
+print('.' + message.strip() + '.') """
+
+""" message = 'brevity is the essence of wit'
+message = message.replace('essence', 'soul')
+print(message) """
+
+message = 'howdy'
+print(message.rjust(20))
+print(message.rjust(20, '-'))
+print(message.ljust(20))
+print(message.ljust(20, '-'))
+
