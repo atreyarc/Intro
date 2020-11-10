fc9f6977bb09a98a30c892efb06f33afacf310b9
atreyarc <atreya-roy.chowdhury@ch.abb.com>
2020-11-10 22:49:15 +0100

    exercise 2 workout

---
+++ python-format-strings/exercise2.py
@@ -0,0 +1,31 @@
+""" message = str.capitalize('first message')
+print(message)
+
+message = 'second message'.capitalize()
+print(message)
+
+message = 'third message'
+print(message.capitalize()) """
+
+""" 
+message = 'hello world'
+print(message.lower())
+print(message.upper())
+
+message = message.title()
+print(message)
+print(message.swapcase()) """
+""" 
+location = 'Mississippi'
+print(location.count('s'))
+
+print(len('how many letters in this string?')) """
+
+message = 'racecar'
+print(message.startswith('r'))
+print(message.startswith('a'))
+print(message.startswith('ra'))
+
+print(message.endswith('r'))
+print(message.endswith('a'))
+print(message.endswith('ar'))
